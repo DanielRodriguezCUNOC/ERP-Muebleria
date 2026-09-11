6a3ab15edb77c21f6b186df71095566cae256749
DanielRodriguezCUNOC <pabloalvarado202130534@cunoc.edu.gt>
2026-09-11 04:24:24 -0600

    Inicio del proyecto

---
+++ Backend/muebleria/src/main/java/com/erp/muebleria/MuebleriaApplication.java
@@ -0,0 +1,13 @@
+package com.erp.muebleria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuebleriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MuebleriaApplication.class, args);
+	}
+
+}
